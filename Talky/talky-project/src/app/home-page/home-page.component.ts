e5faf07f3345ea67dcffdec92c64c2309c9db48b
Rushion Chegge <136695621+Rushion18@users.noreply.github.com>
2023-12-18 16:32:46 +0300

    added home features, made it better

--- Talky/talky-project/src/app/home-page/home-page.component.ts
+++ Talky/talky-project/src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-home-page',
@@ -7,50 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 
 
-export class HomepageComponent implements OnInit {
+export class HomepageComponent {
+
+  @ViewChild('storiesContainer') storiesContainer!: ElementRef;
+
   currentScroll = 0;
-  scrollAmount = 320;
-  maxScroll: number = 0;  // Declare maxScroll as a class property
-
-  // Add any necessary DOM element references here
-  sCont: HTMLElement | null = null;
-  hScroll: HTMLElement | null = null;
-  leftButton: HTMLElement | null = null;
-  rightButton: HTMLElement | null = null;
-
-  ngOnInit(): void {
-    // Initialize your DOM element references here
-    this.sCont = document.querySelector(".stories");
-    this.hScroll = document.querySelector(".status-wrapper");
-    this.leftButton = document.querySelector("#scroll-left");
-    this.rightButton = document.querySelector("#scroll-right");
-
-    if (this.sCont && this.hScroll && this.leftButton && this.rightButton) {
-      this.maxScroll = -this.sCont.offsetWidth + this.hScroll.offsetWidth;
-      this.leftButton.style.opacity = '0';
-
-      // Add any necessary event listeners or call your scroll function here
-    }
-  }
+  scrollAmount = 120;
 
   scrollHorizontal(val: number): void {
-    if (this.sCont && this.leftButton && this.rightButton) {
-      this.currentScroll += val * this.scrollAmount;
-      if (this.currentScroll >= 0) {
-        this.currentScroll = 0;
-        this.leftButton.style.opacity = '0';
-      } else {
-        this.leftButton.style.opacity = '1';
-      }
-      if (this.currentScroll <= this.maxScroll) {
-        this.currentScroll = this.maxScroll;
-        this.rightButton.style.opacity = '0';
-      } else {
-        this.rightButton.style.opacity = '1';
-      }
-      if (this.sCont) {
-        this.sCont.style.left = this.currentScroll + "px";
-      }
-    }
+    this.currentScroll += val * this.scrollAmount;
+
+    // You can set a maximum and minimum scroll value if needed
+    // const maxScroll = ...;
+    // const minScroll = ...;
+    // this.currentScroll = Math.max(minScroll, Math.min(this.currentScroll, maxScroll));
+
+    this.storiesContainer.nativeElement.scrollLeft = this.currentScroll;
   }
 }
+
+
